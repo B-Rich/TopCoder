a2529a61509bdfddd9e360fe36f0b8b24dada8e5
brandon <yangrucheng@gmail.com>
2014-05-14 17:49:01 -0400

    srm144

---
+++ practice/SRM_144_DIV2_550.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class BinaryCode {
+
+public:
+    vector<string> decode(string);
+};
+
+vector<string> Binary::decode(string str) {
+    //string 1
+    
+
+
+}
+
+int main() {
+
+    return 0;
+}
